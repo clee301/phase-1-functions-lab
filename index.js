dd81c20beff5c188a14dc357a0c3ca204d652093
Chan Lee <clee09818@gmail.com>
2022-08-10 00:53:50 -0400

    commit tree f29297463370d212ee806f0265333caceaa19e94

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+  if (location >= 43) {
+    return location - 43 + 1;
+  } else if (location < 43) {
+    return (location - 43 + 1) * -1;
+  }
+}
+
+function distanceFromHqInFeet(location) {
+  return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  const distance = destination - start;
+  if (distance >= 1) {
+    return (destination - start) * 264;
+  } else {
+    return (destination - start) * 264 * -1;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
